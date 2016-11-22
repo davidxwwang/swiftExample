df59de7560f5aeb4601a56add4e8c70b9778faef
wangxingwang <wangxingwang@emoney.cn>
2016-11-22 14:38:32 +0800

    init

--- swiftExample/ViewController.swift
+++ swiftExample/ViewController.swift
@@ -14,7 +14,19 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        var shoppingList = [[]];
+        let names = ["John", "Paul", "George", "Ringo", "Mick", "Keith", "Charlie", "Ronnie"]
+        var stringSet = Set<String>() // 1
+        var loopsCount = 0
+        while stringSet.count < 4 {
+            let randomNumber = arc4random_uniform(UInt32(names.count)) // 2
+            let randomName = names[Int(randomNumber)] // 3
+            print(randomName) // 4
+            stringSet.insert(randomName) // 5
+            loopsCount += 1 // 6
+        }
+    
+        // 7
+        print("Loops: " + loopsCount.description + ", Set contents: " + stringSet.description)
         
     }
 
@@ -23,4 +35,5 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 }
+
 
